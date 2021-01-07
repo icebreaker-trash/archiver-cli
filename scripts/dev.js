541bff828bf5ff6c5a9a48dec6525487cd31c368
icebreaker99 <qq1324318532@gmail.com>
2021-01-07 10:26:03 +0800

    feat: add stat and filename for returns

--- scripts/dev.js
+++ scripts/dev.js
@@ -7,7 +7,8 @@ async function main () {
   // await zip('node_modules')
   const dirpath = path.resolve(__dirname, '../node_modules')
   const destpath = path.resolve(__dirname, '../.serverless/layer/node_modules.zip')
-  await zip(dirpath, destpath)
+  const res = await zip(dirpath, destpath)
+  console.log(res)
   console.log('successfully')
 }
 
