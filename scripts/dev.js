be521007e14f224e3491018a0721e0f1f6d7c869
SonOfMagic <qq1324318532@gmail.com>
2021-01-09 01:01:00 +0800

    fix: add start computing tip and innerPath option

--- scripts/dev.js
+++ scripts/dev.js
@@ -7,7 +7,9 @@ async function main () {
   // await zip('node_modules')
   const dirpath = path.resolve(__dirname, '../node_modules')
   const destpath = path.resolve(__dirname, '../.serverless/layer/node_modules.zip')
-  const res = await zip(dirpath, destpath)
+  const res = await zip(dirpath, destpath, {
+    innerPath: 'node_modules'
+  })
   console.log(res)
   console.log('successfully')
 }
